aa12559fd05ed3278dc365e6ba0cd54f8bdae7be
kkhwjnrk <kukuhwijanarko07@gmail.com>
2023-04-16 01:12:13 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.header');
+const button = document.querySelector('.btn-toggle');
+
+button.addEventListener('click', () => {
+  const isMenuOpen = button.getAttribute('data-toggle') === 'false';
+
+
+  if (isMenuOpen) {
+    button.setAttribute('data-toggle', 'true');
+    button.innerHTML = '<i class="bx bx-menu-alt-right"></i>';
+    menu.classList.remove('header-active');
+  } else {
+    button.setAttribute('data-toggle', 'false');
+    button.innerHTML = '<i class="bx bx-x"></i>';
+    menu.classList.add('header-active');
+  }
+})
