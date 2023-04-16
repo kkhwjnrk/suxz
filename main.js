dc994033aaa17b4d49abf221038141105c168675
kkhwjnrk <kukuhwijanarko07@gmail.com>
2023-04-16 14:15:28 +0700

    update

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const menu = document.querySelector('.header');
 const button = document.querySelector('.btn-toggle');
 
 button.addEventListener('click', () => {
-  const isMenuOpen = button.getAttribute('data-toggle') === 'false';
+  const isMenuOpen = button.getAttribute('data-toggle') === 'true';
 
 
   if (isMenuOpen) {
-    button.setAttribute('data-toggle', 'true');
+    button.setAttribute('data-toggle', 'false');
     button.innerHTML = '<i class="bx bx-menu-alt-right"></i>';
     menu.classList.remove('header-active');
   } else {
-    button.setAttribute('data-toggle', 'false');
+    button.setAttribute('data-toggle', 'true');
     button.innerHTML = '<i class="bx bx-x"></i>';
     menu.classList.add('header-active');
   }
